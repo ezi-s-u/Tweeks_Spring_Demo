9be80f14a6425fd157f49e5a94ff11239facf6fc
ezi-s-u <ljsgoo0617@gmail.com>
2023-08-03 07:39:47 +0900

    init commit

---
+++ src/main/java/tweeks/Tweeks_Spring_Demo/TweeksSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package tweeks.Tweeks_Spring_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeksSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeksSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tweeks/Tweeks_Spring_Demo/TweeksSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tweeks.Tweeks_Spring_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeksSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
